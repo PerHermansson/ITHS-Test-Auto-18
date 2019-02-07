491f92b7675c47a4a0250f471e17d47032e6ebbb
Tony <tonys@mejl.se>
2019-02-07 15:00:37 +0100

    Öppen för ändring

--- Junit/GoodToHaveCheckers/test/ValidatorTestImpl_Shahzad.java
+++ Junit/GoodToHaveCheckers/test/ValidatorTestImpl_Shahzad.java
@@ -5,13 +5,21 @@ import static org.junit.Assert.*;
 public class ValidatorTestImpl_Shahzad extends ValidatorTest {
 
     @Test
-    public void isValidEmail(String email)
-    {
-        email = "contribute@geeksforgeeks.org";
-        if (isValidEmail(email))
-            System.out.print("Yes");
-        else
-            System.out.print("No");
+    public void test_isValidEmail_integer() {
+        boolean result;
+        Validator myValidator = new Validator();
+        result = myValidator.isValidEmail(2345);
+        assertFalse(result);
+
+    }
+
+    @Test
+    public void test_isValidEmail_string() {
+        boolean result;
+        Validator myValidator = new Validator();
+        result = myValidator.isValidEmail("Hej.du.glade");
+        assertTrue(result);
+
     }
 
 
