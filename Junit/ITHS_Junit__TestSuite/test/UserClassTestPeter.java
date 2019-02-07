0569235ecbc9858f60dca8f494975c1b57c6d3ef
Peter Englsperger <peter.englsperger@gmail.com>
2019-02-07 11:35:28 +0100

    renamed file to match project

--- Junit/ITHS_Junit__TestSuite/test/UserClassTestPeter.java
+++ Junit/ITHS_Junit__TestSuite/test/UserClassTestPeter.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class PeterTestClassTest {
+public class UserClassTestPeter{
 
     @Before
     public void setUp() throws Exception {
