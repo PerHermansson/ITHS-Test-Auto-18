9b5a32a847cd8543c941986c770f9b0fb5be000e
Oxana <zakharovaon@mail.ru>
2019-02-07 11:52:44 +0100

    oxana

---
+++ Junit/ITHS_Junit__TestSuite/test/UserClassTest_Oxana.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserClassTest_Oxana {
+
+    @Test
+    public void setA() {
+    }
+
+    @Test
+    public void setB() {
+    }
+
+    @Test
+    public void getUserId() {
+        System.out.println("Oxanas test");
+    }
+
+}
