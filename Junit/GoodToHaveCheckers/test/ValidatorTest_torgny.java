e5df7da33cc0f7f2f5048d243d487108f543f62f
Torgny Lyon <torgny@abc.se>
2019-02-07 14:08:34 +0100

    Lade till isValidEmail_valid() som matas med en giltig adress.
    Lade till en setUp()-metod som instantierar en Validator-klass.

--- Junit/GoodToHaveCheckers/test/ValidatorTest_torgny.java
+++ Junit/GoodToHaveCheckers/test/ValidatorTest_torgny.java
@@ -1,10 +1,20 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ValidatorTest_torgny {
+    private Validator validator;
+
+    @Before
+    public void setUp() {
+        validator = new Validator();
+    }
 
     @Test
-    public void isValidEmail() {
+    public void isValidEmail_valid() {
+        boolean result = validator.isValidEmail("simple@example.com");
+
+        assertTrue(result);
     }
 }
