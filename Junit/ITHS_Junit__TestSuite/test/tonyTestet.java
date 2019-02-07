43714bd11df063d1a5445fe756355001023cc643
Tony <tonys@mejl.se>
2019-02-07 11:41:44 +0100

    Lägger till ett simpelt test

--- Junit/ITHS_Junit__TestSuite/test/tonyTestet.java
+++ Junit/ITHS_Junit__TestSuite/test/tonyTestet.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class tonyTestet {
-}
+
+    @Test
+    public void TESTA() {
+        System.out.println("Tre kanter har min hatt");
+    }
+    }
