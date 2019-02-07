0544155818a943c1108ed100390c2aebc3d93151
Anders83an <44577359+Anders83an@users.noreply.github.com>
2019-02-07 15:08:27 +0100

    Anders

--- Junit/GoodToHaveCheckers/test/ValidatorTest_Anders.java
+++ Junit/GoodToHaveCheckers/test/ValidatorTest_Anders.java
@@ -12,4 +12,45 @@ public class ValidatorTest_Anders {
         assertFalse(result);
 
     }
+    @Test
+    public void test_isValidEmail_string() {
+        boolean result;
+        Validator myValidator = new Validator();
+        result = myValidator.isValidEmail("Hej@du.glade");
+        assertTrue(result);
+
+    }
+
+    @Test
+    public void secondTest() {
+        boolean result;
+        Validator myValidator = new Validator();
+        result = myValidator.isValidEmail("Abc.example.com");
+        assertFalse(result);
+
+    }
+
+    @Test
+    public void thirdTest() {
+        boolean result;
+        Validator myValidator = new Validator();
+        result = myValidator.isValidEmail("A@b@c@example.com");
+        assertTrue(result);
+    }
+
+    @Test
+    public void forthTest() {
+        boolean result;
+        Validator myValidator = new Validator();
+        result = myValidator.isValidEmail("very.common@example.com");
+        assertTrue(result);
+    }
+
+    @Test
+    public void fifthTest() {
+        boolean result;
+        Validator myValidator = new Validator();
+        result = myValidator.isValidEmail("just\"not\"right@example.com");
+        assertTrue(result);
+    }
 }
