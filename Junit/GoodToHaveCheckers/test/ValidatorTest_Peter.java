80bbf834f939adea5f6f1a351e9b2e8731a37352
Peter Englsperger <peter.englsperger@gmail.com>
2019-02-07 14:45:01 +0100

    Added ValidatorTest_Peter

---
+++ Junit/GoodToHaveCheckers/test/ValidatorTest_Peter.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+import java.util.ArrayList;
+
+
+import static org.junit.Assert.*;
+
+public class ValidatorTest_Peter {
+
+    Validator myValidator = new Validator();
+    boolean result;
+
+    ArrayList<String> validEmails = new ArrayList<String>(){{
+        add("simple@example.com");
+        add("very.common@example.com");
+        add("disposable.style.email.with+symbol@example.com");
+        add("other.email-with-hyphen@example.com");
+        add("fully-qualified-domain@example.com");
+        add("user.name+tag+sorting@example.com");
+        add("x@example.com");
+        add("example-indeed@strange-example.com");
+        add("admin@mailserver1");
+        add("example@s.example");
+    }};
+
+    @Test
+    public void test_isValidEmail() {
+        for (String i: validEmails) {
+            result = myValidator.isValidEmail(validEmails);
+            assertFalse(result);
+        }
+
+    }
+
+    @Test
+    public void test_isValidEmail_string() {
+        for (String i : validEmails) {
+            result = myValidator.isValidEmail(validEmails);
+            assertFalse(result);
+        }
+    }
+}
