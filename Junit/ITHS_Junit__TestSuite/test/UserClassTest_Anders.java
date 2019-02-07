38a4c634cb86acefea1c7c7053a7962599f7092c
Anders83an <44577359+Anders83an@users.noreply.github.com>
2019-02-07 11:38:49 +0100

    Anders

---
+++ Junit/ITHS_Junit__TestSuite/test/UserClassTest_Anders.java
@@ -0,0 +1,28 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserClassTest_Anders {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void setA() {
+    }
+
+    @Test
+    public void setB() {
+    }
+
+    @Test
+    public void getUserId() {
+    }
+}
