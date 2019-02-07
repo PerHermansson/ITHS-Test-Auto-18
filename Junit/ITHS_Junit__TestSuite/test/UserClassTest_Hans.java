ef1f695dcd384105e849d6b136892e8b2af34e48
Folklore <h.g.borjesson@gmail.com>
2019-02-07 12:22:31 +0100

    Hans testklass. Nu då!

--- Junit/ITHS_Junit__TestSuite/test/UserClassTest_Hans.java
+++ Junit/ITHS_Junit__TestSuite/test/UserClassTest_Hans.java
@@ -4,7 +4,10 @@ import static org.junit.Assert.*;
 
 public class UserClassTest_Hans {
 
-
+    @Test
+    public void hans(){
+        System.out.println("Kalla mig Hans, sa hans majestät");
+    }
 
 
 }
